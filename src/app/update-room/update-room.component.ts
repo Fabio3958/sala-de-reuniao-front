5273cb20aad9419e5abb3b9e39c57f860fcc2dc4
fabio <fabioevangelista_@hotmail.com>
2023-06-09 14:13:07 -0300

    Componente update room criado

--- src/app/update-room/update-room.component.ts
+++ src/app/update-room/update-room.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Room } from '../room';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RoomService } from '../room.service';
 
 @Component({
   selector: 'app-update-room',
   templateUrl: './update-room.component.html',
   styleUrls: ['./update-room.component.css']
 })
-export class UpdateRoomComponent {
+export class UpdateRoomComponent implements OnInit{
+  
+id:number;
+room:Room;
+submitted = false;
+
+
+constructor(private route: ActivatedRoute, private router: Router, private roomService:RoomService){
+  
+}
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
